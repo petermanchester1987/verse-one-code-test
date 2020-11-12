6c77222523ef1778ff1cc3c0830dacfc5828711e
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-11-12 13:27:28 +0000

    started navbar, added fontawesome cdn to avoid having to use svgs for icons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <header>
-            Nav in here
+           <Navbar bg="light" expand="lg" collapseOnSelect>
+               <Container>
+                    <Navbar.Brand href="/">Robot Shop</Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/products">Products</Nav.Link>
+                            <Nav.Link href="/basket">Basket</Nav.Link> 
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     )
 }
