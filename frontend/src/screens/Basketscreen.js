2c1c34127f9cd98f8305d234e9e090660aeb6cb3
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-11-13 15:26:42 +0000

    added shopping basket section functionality

--- frontend/src/screens/Basketscreen.js
+++ frontend/src/screens/Basketscreen.js
@@ -20,11 +20,73 @@ const Basketscreen = ({ match, location, history }) => {
         if(productId) {
             dispatch(addToBasket(productId, qty))
         }
-    }, [dispatch, productId, qty])
+    }, [dispatch, productId, qty]);
+
+    const removeFromBasketHandler = (id) => {
+        console.log('remove');
+
+    }
 
     return (
         <div>
-            Basket
+            <Row>
+                <Col md={8}>
+                    <h1>Your Shopping Basket</h1>
+                    {basketItems.length === 0 ? (
+                    <div>
+                        <Link to="/" className="btn btn-primary my-5">Back To Products</Link>
+                        <h3>Your Shopping Basket is Empty...</h3> 
+                    </div>
+                    ) : (
+                        <ListGroup variant="flush">
+                            {basketItems.map(item => (
+                                <ListGroup.Item key={item.product}>
+                                    <Row>
+                                        <Col md={3}>
+                                            <Image src={item.image} alt={item.name} fluid></Image>
+                                        </Col>
+                                        <Col md={3}>
+                                            <Link to={`/product/${item.product}`}><h5>{item.name}</h5></Link>
+                                        </Col>
+                                        <Col md={2}>
+                                            £{item.price}
+                                        </Col>
+                                        <Col md={2}>
+                                                <Form.Control as='select' value={item.qty} onChange={(e) => dispatch(addToBasket(item.product, Number(e.target.value)))}>
+                                                   { 
+                                                   [...Array(item.countInStock).keys()].map((x) => (
+                                                        <option key={x + 1} value={x + 1}>{x + 1}</option>
+                                                    ))
+                                                    }
+                                                </Form.Control>
+                                        </Col>
+                                        <Col md={2}>
+                                            <Button type="button" variant="danger" onClick={() => removeFromBasketHandler(item.product)}>
+                                                <i className="fas fa-times"></i>
+                                            </Button>
+                                        </Col>
+                                    </Row>
+
+                                </ListGroup.Item>
+                            ) )}
+                        </ListGroup>
+                    )
+                }
+                </Col>
+                <Col md={4}>
+
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <h4>Subtotal - { basketItems.reduce((a,item) => a + item.qty, 0 )} Items</h4>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+
+
+                </Col>
+                
+            </Row>
         </div>
     )
 }
